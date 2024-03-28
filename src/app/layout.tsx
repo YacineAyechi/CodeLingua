b6772dcb79b03e499bb2bd9c1f6806e18223fda7
YacineAyechi <yacineayachi9@gmail.com>
2024-03-28 02:07:09 +0100

    added web-analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import Navbar from "@/components/Navbar";
 import "./globals.css";
 import Footer from "@/components/Footer";
 import { SessionProvider } from "next-auth/react";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function RootLayout({
   children,
@@ -14,6 +15,7 @@ export default function RootLayout({
         <SessionProvider>
           <Navbar />
           {children}
+          <Analytics />
           <Footer />
         </SessionProvider>
       </body>
